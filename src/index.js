5cfd7111535249273ae62ce30fab03e67528a891
adfe12q <samiraplayerrr@gmail.com>
2026-06-23 19:45:52 +0200

    Set up Discord bot with dotenv configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Bot zalogowany jako ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
